25174e0ab242f503e56a1190ef8779c426c0b269
Kazuto Maeda <a2136km@aiit.ac.jp>
2021-05-22 09:40:40 +0000

    Complete Passowrd reset.

--- app/models/user.rb
+++ app/models/user.rb
@@ -72,6 +72,11 @@ class User < ApplicationRecord
     Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
   
+  # パスワード再設定の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+  
   # ユーザーをフォローする
   def follow(other_user)
     following << other_user
